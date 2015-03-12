de5352aadb0679552dac2ac1c0c1d7c20e6c37f3
Sebastian <metiv9@gmail.com>
2015-03-12 02:01:28 +0100

    draw c.d.

--- web/bundles/framework/javascripts/drawer.js
+++ web/bundles/framework/javascripts/drawer.js
@@ -1,3 +1,4 @@
+
 var mousePressed = false;
 var lastX, lastY;
 var ctx;
@@ -5,17 +6,46 @@ var old_n = Date.now();
 var delay = 25; //ms
 var buffer = new Array();
 
-function InitThis() {
+function Point(x, y){
+    this.x=x;
+    this.y=y;
+}
+
+Point.prototype.setXY = function(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Point.prototype.toString = function() {
+    return "("+this.x+", "+this.y+")";
+}
+
+function Line(x1, y1, x2, y2){
+    this.from = new Point(x1, y1);
+    this.to =  new Point(x2, y2);
+}
+
+Line.prototype.set = function Line(x1, y1, x2, y2){
+    this.from = new Point(x1, y1);
+    this.to =  new Point(x2, y2);
+}
+
+Line.prototype.toString = function() {
+    return "From: "+this.from.toString()+", To: "+this.to.toString();
+}
+
+
+function init() {
     ctx = document.getElementById('canvas').getContext("2d");
 
     $('#canvas').mousedown(function (e) {
         mousePressed = true;
-        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+        draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
     });
 
     $('#canvas').mousemove(function (e) {
         if (mousePressed) {
-            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+            draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
         }
     });
 
@@ -28,23 +58,31 @@ function InitThis() {
     });
 }
 
-function Draw(x, y, isDown) {
+function draw(x, y, isDown) {
     var n = Date.now();
 
     if (n - old_n > delay ) {
         if (isDown) {
-            ctx.beginPath();
-            ctx.strokeStyle = $('#selColor').val();
-            ctx.lineWidth = $('#selWidth').val();
-            ctx.lineJoin = "round";
-            ctx.moveTo(lastX, lastY);
-            ctx.lineTo(x, y);
-            ctx.closePath();
-            ctx.stroke();
+            buffer.push(new Line(lastX, lastY, x, y));
         }
         lastX = x;
         lastY = y;
         old_n=Date.now();
+        clearArea();
+        flush();
+    }
+}
+
+function flush() {
+    for (i = 0; i < buffer.length; i++) {
+        var from = buffer[i].from;
+        var to = buffer[i].to;
+
+        ctx.beginPath();
+        ctx.moveTo(from.x, from.y);
+        ctx.lineTo(to.x, to.y);
+        ctx.closePath();
+        ctx.stroke();
     }
 }
 
@@ -54,7 +92,7 @@ function clearArea() {
 }
 
 function saveImage(path) {
-    var dataURL = document.getElementById('canvas').toDataURL("image/png");
+   /* var dataURL = document.getElementById('canvas').toDataURL("image/png");
     $.ajax({
         type: "POST",
         url: path,
@@ -64,4 +102,10 @@ function saveImage(path) {
     }).done(function(response) {
         alert(response.path);
     });
+
+    (isDown) {
+    buffer.push(JSON.stringify(new Line(lastX, lastY, x, y)));
+var text="";
+
+    alert(text);*/
 }
