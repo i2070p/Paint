9064032168a6202ec3d4ec9d5633c9f896fabfaf
meti <meti@meti-ThinkPad-T500>
2015-07-22 16:52:38 +0200

    up

--- web/bundles/framework/javascripts/Point.js
+++
@@ -1,9 +0,0 @@
-function Point(x, y) {
-    this.x = x;
-    this.y = y;
-}
-
-Point.prototype.setXY = function (x, y) {
-    this.x = x;
-    this.y = y;
-};
--- web/bundles/framework/javascripts/Style.js
+++
@@ -1,9 +0,0 @@
-function Style(color, lineWidth) {
-    this.color = color;
-    this.lineWidth = lineWidth;
-}
-
-Style.prototype.setStyle = function (color, lineWidth) {
-    this.color = color;
-    this.lineWidth = lineWidth;
-};
